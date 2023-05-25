703e59d5f306901ba9d02d2537a49f024e6d6f9b
LookupCz <Lookup17@seznam.cz>
2023-05-25 23:59:09 +0200

    Finish respawn
    yadayada, finally doing smth

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [Header("Movement")]
     [SerializeField]
     private float speed = 2f;
+    [SerializeField]
+    private float getBigger = 0.25f;
 
+    public bool isSlowedDown = false;
+    private float slowDuration = 3.0f;
+
+    [SerializeField]
+    private float slowSpeed = 1f;
+
+    [Header("Particles")]
     [SerializeField]
     private ParticleSystem snowToShrinkEFCT;
 
@@ -19,19 +31,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private ParticleSystem fenceBeingDeEFCT;
 
-    [SerializeField]
-    private float getBigger = 0.25f;
 
     private Rigidbody rb;
     private Camera mainCamera;
 
-    public bool isSlowedDown = false;
-    private float slowDuration = 3.0f;
-    [SerializeField]
-    private float slowSpeed = 0.5f;
+    [Header("Gameplay")]
+    private float timeToSave = 5f;
+
+    private float innerSave;
+    private Vector3 lastPos;
+    private Quaternion lastRot;
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         rb = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
         gameObject.transform.localScale = new Vector3(getBigger, getBigger, getBigger);
@@ -39,6 +54,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+
+        timeToSave -= Time.deltaTime;
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -50,6 +68,8 @@ public class PlayerMovement : MonoBehaviour
         cameraRight.Normalize();
 
         Vector3 movement = (cameraForward * moveVertical + cameraRight * moveHorizontal).normalized;
+
+
 
         if (isSlowedDown)
         {
@@ -88,11 +108,63 @@ public class PlayerMovement : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
+
+        if (timeToSave <= 0 && IsGrounded())
+        {
+            SavePos();
+        }
+
+        if (HasPlayerFell())
+        {
+
+            Respawn();
+        }
     }
 
+    private bool HasPlayerFell()
+    {
+        throw new NotImplementedException();
+        //....
 
+        //check if he fell then respawn him be careful about timing tho
 
+    }
 
+    private void Respawn()
+    {
+        if (gameObject.activeSelf == false)
+        {
+
+            gameObject.SetActive(true);
+            gameObject.transform.position = lastPos;
+            gameObject.transform.rotation = lastRot;
+        }
+    }
+
+    private void SavePos()
+    {
+        Debug.Log("ASOJGAOUIBGOABGAOUBGUYAFCVA");
+
+        lastPos = transform.position;
+        lastRot = transform.rotation;
+        timeToSave += 5f;
+    
+    }
+
+    private bool IsGrounded()
+    {
+        float raycastDistance = 1f;
+        RaycastHit hit;
+        if (Physics.Raycast(new Ray(transform.position, -transform.up), out hit, raycastDistance))
+        {
+            // Check if the hit object is the floor or any other designated tag
+            if (hit.collider.CompareTag("Ground"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
